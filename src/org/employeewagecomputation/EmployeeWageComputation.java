56d2d782ddaf3fb0a8b8ac6042dccde028ad2672
aayush_aryan <log2aayusharyan@gmail.com>
2020-10-16 12:47:13 +0530

    [aayush]added. part time employee and calculating wages by using switch case

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -4,22 +4,21 @@ public class EmployeeWageComputation {
     static int wagePerHour=20;
     static int isPresent=1;
     static int isPartTime = 2;
-    static int empHrs=0;
-    static int empWage=0;
+    static int empHrs;
+    static int empWage;
 
     public static void main(String[] args) {
-        
+
         double randomValue=Math.floor(Math.random()*10)%3;
-        if (randomValue == isPresent)
-        {
-            empHrs=8;
-        }
-        else if(randomValue == isPartTime)
-        {
-            empHrs=4;
-        }
-        else {
-            empHrs=0;
+        switch ((int)randomValue) {
+            case 1:
+                empHrs=8;
+                break;
+            case 2:
+                empHrs=4;
+                break;
+            default:
+                empHrs=0;
         }
         empWage=empHrs*wagePerHour;
         System.out.println("Employee wage is :"+ empWage);
