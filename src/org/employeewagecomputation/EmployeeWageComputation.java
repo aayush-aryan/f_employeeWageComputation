a5cda56536f535bc75e7130aaa27ed16ff9260ab
aayush_aryan <log2aayusharyan@gmail.com>
2020-10-16 20:17:01 +0530

    [aayush]refactor. usecase one

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -2,7 +2,7 @@ package org.employeewagecomputation;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to employeeWageComputation ");
+        System.out.println("Welcome to employeeWageComputation : ");
         final int isPresent=1;
         final double randomValue=Math.floor(Math.random()*10)%2;
         if (randomValue == isPresent)
