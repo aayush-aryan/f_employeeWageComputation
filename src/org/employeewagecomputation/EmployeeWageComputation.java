98c3bf54e9dc107a5f03def557eec5a00b12a4bf
aayush_aryan <log2aayusharyan@gmail.com>
2020-10-16 13:10:14 +0530

    [aayush]added. putting condition For total working hrs and day

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -5,34 +5,44 @@ public class EmployeeWageComputation {
     static int isPresent=1;
     static int isPartTime = 2;
     static int totalWorkingDays=20;
+    static int totalWorkingHrs=100;
     static int empHrs;
+    static int empDay=0;
     static int empWage;
     static int totalEmpHrs=0;
 
     public static void main(String[] args) {
 
-        for (int day= 0; day < totalWorkingDays; day++) {
-
+        while (empDay < totalWorkingDays && totalEmpHrs< totalWorkingHrs) {
 
             double randomValue=Math.floor(Math.random()*10)%3;
             switch ((int)randomValue) {
+
                 case 1:
                     empHrs=8;
+                    if(totalEmpHrs<=92) {
+                        totalEmpHrs=totalEmpHrs+empHrs;
+                        empDay++;
+                    }
+
                     break;
                 case 2:
                     empHrs=4;
+                    if(totalEmpHrs<=96) {
+                        totalEmpHrs=totalEmpHrs+empHrs;
+                        empDay++;
+                    }
                     break;
                 default:
                     empHrs=0;
             }
 
-            totalEmpHrs=totalEmpHrs+empHrs;
-
         }
 
         empWage=totalEmpHrs*wagePerHour;
         System.out.println("Total working hours :" +totalEmpHrs+"hrs");
-        System.out.println("Employee wage is :"+ empWage);
+        System.out.println("Total working days :" +empDay);
+        System.out.println("Total wages of employee :"+ empWage);
 
     }
 }
