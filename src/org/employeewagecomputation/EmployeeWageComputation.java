a8760b8bd102b3f8a34a9b9a4d84e85509acbf16
aayush-aryan <er.ankitaryan@gmail.com>
2020-10-16 11:37:11 +0530

    [aayush]added.Welcome message for employeeWageComputation

---
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package org.employeewagecomputation;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employeeWageComputation ");
+        
+    }
+}
