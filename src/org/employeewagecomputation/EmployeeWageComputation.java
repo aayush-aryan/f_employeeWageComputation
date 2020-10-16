8e7cbcdce44f4a6d70c1ae67ee9e75b746265ebc
aayush-aryan <er.ankitaryan@gmail.com>
2020-10-16 11:41:34 +0530

    [aayush]added. calulating employee is present or not by using Random function

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -3,6 +3,16 @@ package org.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to employeeWageComputation ");
-        
+        final int isPresent=1;
+        final double randomValue=Math.floor(Math.random()*10)%2;
+        if (randomValue == isPresent)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+
+        }
     }
 }
