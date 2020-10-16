1216b42f8ef8d993abc1ee8ef20f7c44c1d6b640
aayush_aryan <log2aayusharyan@gmail.com>
2020-10-16 12:58:26 +0530

    [aayush]added. calculating wage for a month

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -4,23 +4,34 @@ public class EmployeeWageComputation {
     static int wagePerHour=20;
     static int isPresent=1;
     static int isPartTime = 2;
+    static int totalWorkingDays=20;
     static int empHrs;
     static int empWage;
+    static int totalEmpHrs=0;
 
     public static void main(String[] args) {
 
-        double randomValue=Math.floor(Math.random()*10)%3;
-        switch ((int)randomValue) {
-            case 1:
-                empHrs=8;
-                break;
-            case 2:
-                empHrs=4;
-                break;
-            default:
-                empHrs=0;
+        for (int day= 0; day < totalWorkingDays; day++) {
+
+
+            double randomValue=Math.floor(Math.random()*10)%3;
+            switch ((int)randomValue) {
+                case 1:
+                    empHrs=8;
+                    break;
+                case 2:
+                    empHrs=4;
+                    break;
+                default:
+                    empHrs=0;
+            }
+
+            totalEmpHrs=totalEmpHrs+empHrs;
+
         }
-        empWage=empHrs*wagePerHour;
+
+        empWage=totalEmpHrs*wagePerHour;
+        System.out.println("Total working hours :" +totalEmpHrs+"hrs");
         System.out.println("Employee wage is :"+ empWage);
 
     }
