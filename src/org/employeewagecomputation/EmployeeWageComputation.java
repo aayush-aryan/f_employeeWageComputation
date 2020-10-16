c0e22b0a542499a4de92e19528b0ac03190b9da6
aayush_aryan <log2aayusharyan@gmail.com>
2020-10-16 12:11:07 +0530

    [aayush]added. part time employee and calculating wages

--- src/org/employeewagecomputation/EmployeeWageComputation.java
+++ src/org/employeewagecomputation/EmployeeWageComputation.java
@@ -3,18 +3,22 @@ package org.employeewagecomputation;
 public class EmployeeWageComputation {
     static int wagePerHour=20;
     static int isPresent=1;
+    static int isPartTime = 2;
     static int empHrs=0;
     static int empWage=0;
 
     public static void main(String[] args) {
-
-        double randomValue=Math.floor(Math.random()*10)%2;
+        
+        double randomValue=Math.floor(Math.random()*10)%3;
         if (randomValue == isPresent)
         {
             empHrs=8;
         }
-        else
+        else if(randomValue == isPartTime)
         {
+            empHrs=4;
+        }
+        else {
             empHrs=0;
         }
         empWage=empHrs*wagePerHour;
